8b3d4a6e488846553b3a8faf73430c8773b82422
Tarek994 <ghali.tarek.123@hotmail.com>
2023-05-04 23:42:05 +0300

    feat(index.js): add range_slider_integer function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const integer = require('input-integer_tar')
 
 module.exports = range_slider_integer
 
+function range_slider_integer (opts){
+
+    const el = document.createElement('div')
+    const shadow = el.attachShadow({mode: 'closed'})
+
+    const range_slider =range(opts)
+    const input_integer =integer(opts)
+
+    shadow.append(range_slider, input_integer)
+
+    return el
+}
