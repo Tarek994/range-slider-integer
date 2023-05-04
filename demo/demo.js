836d72fd83673cea0e391f6cf94b7ea7890ec525
Tarek994 <ghali.tarek.123@hotmail.com>
2023-05-04 23:35:37 +0300

    feat(demo): add integer range slider to the body

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,6 @@
-const range = require ('range-slider_tar')
-const integer = require ('input-integer_tar')
-
+const range_slider_integer = require('..')
 
 const opts = { min: 0, max: 10 }
-const range_slider = range(opts)
-const input_integer = integer(opts)
+const rsi = range_slider_integer(opts)
 
-document.body.append(range_slider, input_integer)
+document.body.append(rsi)
